ba6014e2cbfa334deb9dafc5ae4a5fe65787a022
vibhore tiwari <vibhore.tiwari@coraltele.com>
2024-10-01 15:25:50 +0530

    initialize

---
+++ chatappserverside/src/main/java/com/example/chatappserverside/ChatappserversideApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatappserverside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatappserversideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatappserversideApplication.class, args);
+	}
+
+}
---
+++ chatappserverside/src/test/java/com/example/chatappserverside/ChatappserversideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatappserverside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappserversideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
